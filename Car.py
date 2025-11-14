8bace867149aea06c9a75f55e93a1d27bcae1cd5
kurt <kurt.ishisaka@gmail.com>
2025-11-14 08:49:43 -0800

    change_steering_model

--- Car.py
+++ Car.py
@@ -17,6 +17,8 @@ class Car(UnitObject):
         self.fire_rate = 2
         self.current_fire_rate_limiter = 1
         self.last_known_position = []
+        self.selected_weapon = 0
+        self.set_direction = 0
         surface = pygame.Surface((self.width, self.height), pygame.SRCALPHA)
         pygame.draw.rect(surface, (255, 0, 0), (0, 0, self.width, self.height))
         pygame.draw.line(surface, (0, 255, 0), (0, 18), (18, 18))
@@ -58,29 +60,56 @@ class Car(UnitObject):
         self.rect.y = self.y + y_update
 
     def update_direction(self, other_cars):
-        self.previous_direction = self.current_direction
-        if self.is_turning_left:
-            self.current_direction = self.current_direction + self.turn_mode
-            if self.current_direction > 360:
-                self.current_direction = 0
-        if self.is_turning_right:
+        leftCount = 0
+        currentDirection = self.current_direction
+        while self.set_direction != currentDirection:
+            currentDirection = currentDirection - 1
+            leftCount = leftCount + 1
+            if currentDirection < 0:
+                currentDirection = 360
+
+        rightCount = 0
+        currentDirection = self.current_direction
+        while self.set_direction  != currentDirection:
+            currentDirection = currentDirection + 1
+            rightCount = rightCount + 1
+            if currentDirection > 360:
+                currentDirection = 0
+
+        if leftCount < rightCount:
             self.current_direction = self.current_direction - self.turn_mode
             if self.current_direction < 0:
                 self.current_direction = 360
+        else:
+            self.current_direction = self.current_direction + self.turn_mode
+            if self.current_direction > 360:
+                self.current_direction = 0
+
+        # self.previous_direction = self.current_direction
+        # if self.is_turning_left:
+        #     self.current_direction = self.current_direction + self.turn_mode
+        #     if self.current_direction > 360:
+        #         self.current_direction = 0
+        # if self.is_turning_right:
+        #     self.current_direction = self.current_direction - self.turn_mode
+        #     if self.current_direction < 0:
+        #         self.current_direction = 360
 
     def update_acceleration(self):
-        if self.is_accelerating:
-            self.current_acceleration = self.current_acceleration + .001
-        elif self.is_decelerating:
-            self.current_acceleration = self.current_acceleration - .001
-        elif not self.is_accelerating and not self.is_decelerating:
-            self.current_acceleration = 0
+        pass
+        # if self.is_accelerating:
+        #     self.current_acceleration = self.current_acceleration + .001
+        # elif self.is_decelerating:
+        #     self.current_acceleration = self.current_acceleration - .001
+        # elif not self.is_accelerating and not self.is_decelerating:
+        #     self.current_acceleration = 0
 
     def update_speed(self):
-        self.current_speed = self.current_speed + self.current_acceleration
-        # speed limits
-        if self.current_speed > self.max_speed:
-            self.current_speed = self.max_speed
-        elif self.current_speed < -self.max_speed:
-            self.current_speed = -self.max_speed
+        pass
+        # self.current_speed = self.current_speed + self.current_acceleration
+        # # speed limits
+        # if self.current_speed > self.max_speed:
+        #     self.current_speed = self.max_speed
+        # elif self.current_speed < -self.max_speed:
+        #     self.current_speed = -self.max_speed
 
--- UnitObject.py
+++ UnitObject.py
@@ -4,6 +4,7 @@ class UnitObject:
         self.name = name
         self.current_speed = 0.0
         self.max_speed = max_speed
+        self.max_throttle = .25
         self.acceleration = acceleration
         self.current_acceleration = current_acceleration
         self.internal_hull = 100
@@ -16,7 +17,7 @@ class UnitObject:
         self.y = y
         self.current_direction = 0
         self.previous_direction = 0
-        self.turn_mode = 2
+        self.turn_mode = 3
         self.surface = image
         self.rect = hitbox
         self.fuel = -1
